7ab2c790216078800a76387438389b89e022f712
sayunlopchan <sayunlopchan01+github@gmail.com>
2025-02-05 16:56:48 +0545

    s

--- src/js/showMore.js
+++ src/js/showMore.js
@@ -1,12 +1,15 @@
 document.getElementById('toggle-button').addEventListener('click', function () {
   const extraParagraph = document.getElementById('extra-paragraph');
+  const ellipsis = document.getElementById('ellipsis');
   const button = this;
 
   if (extraParagraph.style.display === 'none' || extraParagraph.style.display === '') {
     extraParagraph.style.display = 'block';
+    ellipsis.style.display = 'none'; // Hide the "......" when the full content is revealed
     button.innerHTML = `Show Less <img src="src/assets/SVG files/big-arrow.svg" alt="right-arrow">`;
   } else {
     extraParagraph.style.display = 'none';
+    ellipsis.style.display = 'inline'; // Show the "......" when the content is hidden
     button.innerHTML = `Learn More <img src="src/assets/SVG files/big-arrow.svg" alt="right-arrow">`;
   }
 });
