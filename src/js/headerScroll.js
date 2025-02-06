a2d3f2e0f470550ecdf4c695a3ec514989bce147
sayunlopchan <sayunlopchan01+github@gmail.com>
2025-02-06 16:23:46 +0545

    navmi

--- src/js/headerScroll.js
+++ src/js/headerScroll.js
@@ -1,31 +1,16 @@
 let header = document.getElementById("header");
 let fixed = header.offsetTop;
 
-
-function applySavedState() {
-  const isHeaderFixed = localStorage.getItem("isHeaderFixed");
-  if (isHeaderFixed === "true") {
-    header.classList.add("fixed");
-    header.style.backgroundColor = "#f590326e";
-  } else {
-    header.classList.remove("fixed");
-    header.style.backgroundColor = "var(--primary)";
-  }
-}
-
+// Function to make the header fixed on scroll
 function makeHeaderFixed() {
   if (window.scrollY > fixed + 1) {
     header.classList.add("fixed");
     header.style.backgroundColor = "#f590326e";
-    localStorage.setItem("isHeaderFixed", "true");
   } else {
     header.classList.remove("fixed");
     header.style.backgroundColor = "var(--primary)";
-    localStorage.setItem("isHeaderFixed", "false");
   }
 }
 
-
-applySavedState();
-
+// Add scroll event listener
 window.addEventListener("scroll", makeHeaderFixed);
